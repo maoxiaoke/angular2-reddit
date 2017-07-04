f8eb7394f1e6e40fe68cf319094f576a70feb930
maoxiaoke <maoxiaoke@outlook.com>
2017-07-04 10:41:56 +0800

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import {Article} from './article/article.model'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  articles: Article[];
+
+  constructor(){
+    this.articles = [
+      new Article('Angular 2', 'http://xiaokedada.com', 10),
+      new Article('timi 2', 'http://xiaokedada.com', 2),
+      new Article('yuer 2', 'http://xiaokedada.com', 3)
+    ];
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log (`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
+}
